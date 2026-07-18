c8ee276d55116b1462f05d690f1a3b99abe29e9e
Fringiskius <giginski875@gmail.com>
2026-07-18 17:00:08 +0100

    primo commit

---
+++ src/main/java/it/uniroma3/siw/model/Commento.java
@@ -0,0 +1,50 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+@Entity
+public class Commento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Lob
+    private String immagineBase64;
+
+    @Column(length = 1000)
+    private String testo;
+    
+    public Commento() {
+    }
+    
+    public String getImmagineBase64() {
+        return immagineBase64;
+    }
+
+    public void setImmagineBase64(String immagineBase64) {
+        this.immagineBase64 = immagineBase64;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+}
